cb224168ae0e8476bae4736bec3c7445e90a34fb
PYLYP CHELBARAKH <143128807+mrignis@users.noreply.github.com>
2023-11-26 11:03:04 -0500

    UP

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,21 @@
+const profile = {
+  username: 'Jacob',
+  playTime: 300,
+  changeUsername: function (newName) {
+    this.username = newName;
+  },
+  updatePlayTime: function (hours) {
+    this.playTime += hours;
+  },
+  getInfo: function () {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+};
 
-function checkForSpam(message) {
-const lowerCaseMessage = message.toLowerCase();
-return lowerCaseMessage.includes('spam') || lowerCaseMessage.includes('sale');
-}
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername('Marco');
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
